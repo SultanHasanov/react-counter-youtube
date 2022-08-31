04f73581edc849dc61f3f878c1c1933985f59963
sultan <sultanhasanov987@gmail.com>
2022-08-31 12:25:29 +0300

    Сделал обычный счетчик

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+import React from 'react';
+import { useState } from 'react';
+
+const App = () => {
+
+  const [count, setCount] = useState(0)
+
+
+  const handleDec = () => {
+    setCount(count - 1)
+  }
+
+  const handleInc = () => {
+    setCount(count + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='counter'>
+        <div className='count'>{count}</div>
+        <button className='btn_dec' onClick={handleDec}>-</button>
+        <button className='btn_inc' onClick={handleInc}>+</button>
     </div>
   );
-}
+};
 
 export default App;
