8fd81ff8779a542aa317ccf9635ae15b76ebb454
sultan <sultanhasanov987@gmail.com>
2024-12-17 00:04:37 +0300

    Сделал рефакторинг

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,68 @@
 import "./App.css";
-
-import React from 'react';
 import { useState } from "react";
 
 const App = () => {
-   
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [textError, setTextError] = useState(false);
 
   const handleDec = () => {
-    setCount(count - 1)
-  }
+    if (count <= 0) {
+      setTextError(true);
+    } else {
+      setCount((prevCount) => prevCount - 1);
+      setTextError(false);
+    }
+  };
 
   const handleInc = () => {
-    setCount(count + 1);
+    setCount((prevCount) => prevCount + 1);
+    setTextError(false);
   };
 
   const handleMinus2 = () => {
-    setCount(count - 2);
+    if (count <= 1) {
+      setTextError(true);
+    } else {
+      setCount((prevCount) => prevCount - 2);
+      setTextError(false);
+    }
   };
 
   const handlePlus5 = () => {
-    setCount(count + 5);
+    setCount((prevCount) => prevCount + 5);
+    setTextError(false);
   };
 
   const handleReset = () => {
     setCount(0);
+    setTextError(false);
   };
-
-
 
   return (
     <div className="counter">
       <div className="count">
         <div>{count}</div>
-        <button className="btn_dec" onClick={handleDec}>-</button>
-        <button className="btn_inc" onClick={handleInc}>+</button>
-        <button className="btn_minus_2" onClick={handleMinus2}>2-</button>
-        <button className="btn_plus_5" onClick={handlePlus5}>5+</button>
-        <button className="btn_reset" onClick={handleReset}>reset</button>
+        <button className="btn_dec" onClick={handleDec}>
+          -
+        </button>
+        <button className="btn_inc" onClick={handleInc}>
+          +
+        </button>
+        <button className="btn_minus_2" onClick={handleMinus2}>
+          2-
+        </button>
+        <button className="btn_plus_5" onClick={handlePlus5}>
+          5+
+        </button>
+        <button className="btn_reset" onClick={handleReset}>
+          reset
+        </button>
       </div>
+      {textError && (
+        <span className="error">
+          Ошибка: значение не может быть меньше нуля!
+        </span>
+      )}
     </div>
   );
 };
